6096a8b0eca4957c10016a5758378edba754651f
Hector Flores <hecto932@gmail.com>
2019-03-09 02:49:12 -0600

    Polimorfismo

--- Program.cs
+++ Program.cs
@@ -13,8 +13,25 @@ namespace CoreSchool
       var schoolEngine = new SchoolEngine();
       schoolEngine.initialization();
       Printer.DrawTitle("Welcome to the School");
-
       printSchoolCourses(schoolEngine.School);
+
+      Printer.DrawLine(20);
+      Printer.DrawLine(20);
+      Printer.DrawLine(20);
+      Printer.DrawTitle("Testing polimorfismo");
+      var studentTest = new Student{Name="Claire Underwood"};
+
+      ObjSchoolBase ob = studentTest;
+      Printer.DrawTitle("Student");
+      WriteLine($"Student: {studentTest.Name}");
+      WriteLine($"Student: ${studentTest.UniqueId}");
+
+      Printer.DrawTitle("ObjSchoolBase");
+      WriteLine($"Student: {ob.Name}");
+      WriteLine($"Student: {ob.UniqueId}");
+
+      // var objDummy = new ObjSchoolBase(){Name="Frank Underwood"};
+
     }
 
 
