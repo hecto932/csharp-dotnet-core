8dc8e983f7613cbe8d9643cb488f4bdedbfb57f5
Hector Flores <hecto932@gmail.com>
2019-03-13 03:20:34 -0600

    Divide y triunfaras

--- App/Reporter.cs
+++ App/Reporter.cs
@@ -5,38 +5,48 @@ using CoreSchool.Entities;
 
 namespace CoreSchool.App
 {
-  public class Reporter {
+  public class Reporter
+  {
 
     Dictionary<DictionaryKeys, IEnumerable<ObjSchoolBase>> _dictionary;
-    public Reporter (Dictionary<DictionaryKeys, IEnumerable<ObjSchoolBase>> dicObjSchool) {
-      if (dicObjSchool == null) {
+    public Reporter(Dictionary<DictionaryKeys, IEnumerable<ObjSchoolBase>> dicObjSchool)
+    {
+      if (dicObjSchool == null)
+      {
         throw new ArgumentNullException(nameof(dicObjSchool));
       }
       _dictionary = dicObjSchool;
     }
 
-    public IEnumerable<Evaluation> GetEvaluationList() {
-      if (_dictionary.TryGetValue(DictionaryKeys.Evaluation, out IEnumerable<ObjSchoolBase> list) == true) {
+    public IEnumerable<Evaluation> GetEvaluationList()
+    {
+      if (_dictionary.TryGetValue(DictionaryKeys.Evaluation, out IEnumerable<ObjSchoolBase> list) == true)
+      {
         return list.Cast<Evaluation>();
-      } else {
+      }
+      else
+      {
         return new List<Evaluation>();
       }
     }
 
-    public IEnumerable<string> GetSubjectList() {
+    public IEnumerable<string> GetSubjectList()
+    {
       var listEvaluations = GetEvaluationList();
 
       return GetSubjectList(out var dummy);
     }
 
-    public IEnumerable<string> GetSubjectList(out IEnumerable<Evaluation> listEvaluations) {
+    public IEnumerable<string> GetSubjectList(out IEnumerable<Evaluation> listEvaluations)
+    {
       listEvaluations = GetEvaluationList();
 
       return (from Evaluation ev in listEvaluations
               select ev.Subject.Name).Distinct();
     }
 
-    public Dictionary<string, IEnumerable<Evaluation>> GetEvaluationDictBySubject () {
+    public Dictionary<string, IEnumerable<Evaluation>> GetEvaluationDictBySubject()
+    {
       var dicResponse = new Dictionary<string, IEnumerable<Evaluation>>();
 
       var listSubjects = GetSubjectList(out var listEval);
@@ -44,13 +54,30 @@ namespace CoreSchool.App
       foreach (var s in listSubjects)
       {
         var evalsSubj = from eval in listEval
-                          where eval.Subject.Name == s
-                          select eval;
-                          
-          dicResponse.Add(s, evalsSubj);
+                        where eval.Subject.Name == s
+                        select eval;
+
+        dicResponse.Add(s, evalsSubj);
       }
 
       return dicResponse;
     }
+
+    public Dictionary<string, IEnumerable<object>> GetStudentAverageBySubject() {
+      var response = Dictionary<string, IEnumerable<object>>();
+
+      var dicEvaluationBySubject = GetEvaluationDictBySubject();
+      
+      foreach (var subjectWithEval in dicEvaluationBySubject)
+      {
+          var dummy = from eval in subjectWithEval.Value
+                      select new{ 
+                        eval.Subject.UniqueId,
+                        eval.Points
+                      };
+      }
+
+      return response;
+    } 
   }
 }
